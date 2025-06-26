773a5a5325e917eeae4fab3a1c37e257bf67c144
Camilla82 <camillabertini@gmail.com>
2025-06-26 15:52:37 +0100

    made sode minor changes to tests, mostly comments

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -1,27 +1,32 @@
 import pytest
 import os
+
 from unittest.mock import patch
 from sqlalchemy import create_engine
-import sqlalchemy
 
-@patch("sqlalchemy.create_engine")
-@patch("os.path.exists")
-@patch("os.remove")
-def test_db_is_initialised(mock_exists, mock_remove, mock_create_engine):
 
-    # set up mocks
+class TestConnection:
+    #replace the real functions with mock objects 
+    @patch("sqlalchemy.create_engine")
+    @patch("os.remove")
+    @patch("os.path.exists")
 
-    mock_exists.return_value = True # my file exists
-    
-    if os.path.exists("my_db.db"):
-        os.remove("my_db.db")
+    #test method that checks your database setup logic
+    def test_db_is_initialised(self, mock_exists, mock_remove, mock_create_engine):
 
-    sqlalchemy.create_engine("postgresql+pg8000://postgre:password@localhost/mydatabase", echo=True)
+        
+        mock_exists.return_value = True # forcing to return true - file exists
+        
+        if os.path.exists("my_db.db"):
+            os.remove("my_db.db") # no actual file is deleted
 
-    mock_exists.assert_called_once_with("my_db.db")
-    mock_remove.assert_called_once_with("my_db.db")
-    mock_create_engine.assert_called_once_with("postgresql+pg8000://postgre:password@localhost/mydatabase",
-        echo=True
-    )
+        # engine is created
+        sqlalchemy.create_engine("postgresql+pg8000://postgre:password@localhost/mydatabase", echo=True)
+
+        mock_exists.assert_called_once_with("my_db.db") #check for the file’s existence.
+        mock_remove.assert_called_once_with("my_db.db") #check if the file is removed
+        mock_create_engine.assert_called_once_with("postgresql+pg8000://postgre:password@localhost/mydatabase",
+            echo=True #check if the engine is created
+        )
 
         
