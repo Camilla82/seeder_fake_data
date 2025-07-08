75fcbd1a3b47f4c1c1f4c1d4530141eb60eb0155
Camilla82 <camillabertini@gmail.com>
2025-07-08 19:25:02 +0100

    added fake data and tests

---
+++ src/seed.py
@@ -0,0 +1,130 @@
+from src.utils import init_engine
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, Numeric, Boolean, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy.sql import func
+from sqlalchemy.exc import SQLAlchemyError
+from faker import Faker
+import os 
+import random 
+
+# Define a PostgreSQL connection string
+
+
+if os.path.exists("my_db.db"):
+        os.remove("my_db.db")
+
+pg8000engine = init_engine() # see utils
+
+# declarative models 
+# SQLAlchemy generates the appropriate SQL
+#https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/basic_use.html
+# declarative_base Construct a base class for declarative class definitions.
+# sales schema: 
+Base = declarative_base() # all mapped classes should inherit this base class
+
+# class SalesOrder(Base):
+#     __tablename__ = 'sales_order'
+#     pass
+
+# class Counterparty(Base):
+#     __tablename__ = 'counterparty'
+#     pass
+
+# class Currency(Base):
+#     __tablename__ = 'currency'
+#     pass
+
+# class Date(Base):
+#     __tablename__ = 'date'
+#     pass
+
+class Department(Base):
+    __tablename__ = 'department'
+    department_id = Column(Integer, primary_key=True)
+    department_name = Column(String)
+    
+# class Design(Base):
+#     __tablename__ = 'design'
+#     pass
+
+# class Location(Base):
+#     __tablename__ = 'location'
+#     pass
+
+#https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
+#https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html 
+#https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#declarative-table-with-mapped-column
+class Staff(Base):
+    __tablename__ = 'staff'
+    staff_id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    department_id = Column(Integer, ForeignKey('department.department_id'), nullable=False)
+    email_address = Column(String, nullable=False)
+    # timestamp columns 
+    #https://docs.sqlalchemy.org/en/20/core/defaults.html#client-invoked-sql-expressions
+    # created_at = Column(DateTime, default=func.now()) # add created at column
+    last_updated = Column(DateTime, default=func.now()) 
+    created_at = Column(DateTime, default=func.now()) # add created at column
+    last_updated = Column(DateTime, default=func.now()) # add last updated column
+# Drop and create tables
+# https://docs.sqlalchemy.org/en/20/core/metadata.html#sqlalchemy.schema.MetaData.drop_all
+# https://docs.sqlalchemy.org/en/20/core/metadata.html#sqlalchemy.schema.MetaData.create_all
+
+# metadata holds information about all the ORM-mapped tables declared 
+#https://docs.sqlalchemy.org/en/13/core/metadata.html#sqlalchemy.schema.MetaData
+try:
+    Base.metadata.drop_all(pg8000engine)  #drop all the know tables to Base.metadata
+    Base.metadata.create_all(pg8000engine) # create all the know tables to Base.metadata
+except SQLAlchemyError as e:
+    print("Failed to drop and create tables:", e)
+    raise
+
+# SET UP SESSION
+#https://docs.sqlalchemy.org/en/20/orm/session_basics.html
+#https://docs.sqlalchemy.org/en/20/orm/quickstart.html#create-objects-and-persist
+#https://www.youtube.com/watch?v=529LYDgRTgQ&t=2287s&ab_channel=NeuralNine
+
+Session = sessionmaker(pg8000engine)
+session = Session()
+
+fake = Faker()
+
+
+# add x departments names
+
+departments = [
+    Department(department_id=1, department_name="Human Resources"),
+    Department(department_id=2, department_name="Finance"),
+    Department(department_id=3,department_name="Marketing"),
+    Department(department_id=4,department_name="Research & Development")
+]
+
+session.add_all(departments)
+session.commit()
+
+#https://faker.readthedocs.io/
+#add staff 
+## loop? for each entry add this
+#https://docs.sqlalchemy.org/en/14/orm/query.html
+departments = session.query(Department).all() # fetchin all the rows
+
+for _ in range(200):
+    # random department https://docs.python.org/3/library/random.html
+    # create staff member 
+    dept = random.choice(departments)
+    staff = Staff(
+         first_name=fake.first_name(),
+         last_name=fake.last_name(),
+         department_id=dept.department_id,
+         email_address=fake.email(),
+    )
+
+    session.add(staff)
+
+session.commit()
+
+# CLOSE SESSION
+
+
+session.close()
---
+++ tests/test_seed.py
@@ -0,0 +1,185 @@
+import pytest
+import os
+
+from src.seed import Staff, Base, Department, pg8000engine
+from src.utils import init_engine 
+from unittest.mock import patch
+from sqlalchemy import text, create_engine, Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from faker import Faker
+import datetime
+import random
+
+fake = Faker() 
+
+class TestConnection:
+    #replace the real functions with mock objects 
+    #https://docs.python.org/3/library/unittest.mock.html 
+    #https://realpython.com/python-mock-library/
+    # order matters!
+    @patch("src.utils.sqlalchemy_create_engine")
+    @patch("os.remove")
+    @patch("os.path.exists")
+    def test_db_is_initialised(self, mock_exists, mock_remove, mock_create_engine):
+
+        mock_exists.return_value = True # forcing to return true - file exists
+        
+        if os.path.exists("my_db.db"):
+            os.remove("my_db.db") # no actual file is deleted
+
+        # engine is created
+        #sqlalchemy.create_engine("postgresql+pg8000://postgre:password@localhost/mydatabase", echo=True)
+        init_engine("postgresql+pg8000://postgre:password@localhost/mydatabase", echo=True)
+
+        mock_exists.assert_called_once_with("my_db.db") #check for the file’s existence.
+        mock_remove.assert_called_once_with("my_db.db") #check if the file is removed
+        mock_create_engine.assert_called_once_with("postgresql+pg8000://postgre:password@localhost/mydatabase",
+            echo=True #check if the engine is created
+        )
+
+    @patch("src.utils.sqlalchemy_create_engine")
+    @patch("os.remove")
+    @patch("os.path.exists")
+    def test_error_engine_not_created(self, mock_exists, mock_remove, mock_create_engine):
+        mock_exists.return_value = False
+        mock_create_engine.side_effect = SQLAlchemyError("Mocked connection error")
+
+        with pytest.raises(SQLAlchemyError, match="Mocked connection error"):
+            init_engine("postgresql+pg8000://postgre:password@localhost/mydatabase", echo=True)
+
+class TestStaff:
+    # Create fixture for connection 
+    @pytest.fixture
+    def session(self):
+        # Create SQLite database
+        # engine = create_engine("sqlite:///:memory:")
+        # session  https://docs.sqlalchemy.org/en/13/orm/session_api.html#sqlalchemy.orm.session.sessionmaker
+        engine = create_engine("postgresql+pg8000://postgres:password@localhost/mydatabase")
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        
+        session.query(Staff).delete() #Foreign Key Constraint Violation - careful
+        session.commit()
+        session.query(Department).delete()
+        session.commit()
+        
+        # Reset the department_id sequence to 1
+        session.execute(text("ALTER SEQUENCE department_department_id_seq RESTART WITH 1"))
+        session.commit()
+
+        yield session
+        session.close()
+
+    def test_create_staff(self, session):
+
+        # Create department from Department class
+        department = Department(department_id=1, department_name="Engineering") 
+        session.add(department) 
+
+        # Create and add a staff instance
+        staff = Staff(
+            first_name="Camilla",
+            last_name="Bertini",
+            department_id=1,
+            email_address="camillabertini@fakemail.com"
+        )
+        session.add(staff)
+
+        session.commit()
+
+        # session query SELECT and returns first row
+        #https://docs.sqlalchemy.org/en/20/orm/queryguide/query.html#sqlalchemy.orm.Query.first 
+        saved = session.query(Staff).first()
+
+        # Checking if data has been entered correctly
+        assert saved.first_name == "Camilla"
+        assert saved.last_name == "Bertini"
+        assert saved.department_id == 1
+        assert saved.email_address == "camillabertini@fakemail.com"
+
+        # checking right data type
+        assert isinstance(saved.first_name, str)
+        assert isinstance(saved.last_name, str)
+        assert isinstance(saved.department_id, int)
+        assert isinstance(saved.email_address, str)
+
+        # checking timestamps are created
+        assert isinstance(saved.created_at, datetime.datetime)
+        assert isinstance(saved.last_updated, datetime.datetime)
+
+class TestInsert:
+    @pytest.fixture
+    def session(self):
+        # Create SQLite database
+        # engine = create_engine("sqlite:///:memory:")
+        # session  https://docs.sqlalchemy.org/en/13/orm/session_api.html#sqlalchemy.orm.session.sessionmaker
+        engine = create_engine("postgresql+pg8000://postgres:password@localhost/mydatabase")
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        
+        session.query(Staff).delete()
+        session.commit()
+        session.query(Department).delete()
+        session.commit()
+
+        # Reset the department_id sequence to 1
+        session.execute(text("ALTER SEQUENCE department_department_id_seq RESTART WITH 1"))
+        session.execute(text("ALTER SEQUENCE staff_staff_id_seq RESTART WITH 1"))
+        session.commit()
+
+        yield session
+        session.close()
+    
+    def test_if_departments_have_been_inserted_correctly(self, session):
+
+        # Create department from Department class
+        #department = Department(department_id=1, department_name="Engineering") 
+        departments = [
+            Department(department_name="Human Resources"),
+            Department(department_name="Finance"),
+            Department(department_name="Marketing"),
+            Department(department_name="Research & Development")
+        ]
+        session.add_all(departments)
+
+        session.commit()
+
+        result = session.query(Department).all()
+
+        assert len(result) == 4
+        assert result[0].department_name == "Human Resources" 
+
+    def test_inserting_200_fake_staff_entries(self,session):
+
+        departments = [
+            Department(department_name="Human Resources"),
+            Department(department_name="Finance"),
+            Department(department_name="Marketing"),
+            Department(department_name="Research & Development")
+        ]
+        session.add_all(departments)
+
+        session.commit()
+
+        # Generate 200 staff
+        for _ in range(200):
+            dept = random.choice(departments)
+            staff = Staff(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                department_id=dept.department_id,
+                email_address=fake.email()
+            )
+            session.add(staff)
+
+        session.commit()
+
+        # assert 200 staff members
+
+        result = session.query(Staff).all() #fetch staff
+
+        assert len(result) == 200
+
